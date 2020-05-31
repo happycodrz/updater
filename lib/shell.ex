4123dca037465e40557c39ba7fe16358c1d923ed
Roman Heinrich <roman.heinrich@gmail.com>
2020-05-31 13:57:11 +0200

    Change GIT cloning command to stream output
    Problem:
    - in the case there is a network problem, it is not very transparent why
    something didn't work
    
    Solution:
    - since it mostly happens on initial cloning, use the System.cmd
    function with streaming output for this case
    - use the :os.cmd for the update command

--- lib/shell.ex
+++ lib/shell.ex
@@ -1,5 +1,19 @@
 defmodule Updater.Shell do
   def run(cmd) do
+    if String.contains?(cmd, "reset --hard") do
+      blocking_run(cmd)
+    else
+      streaming_run(cmd)
+    end
+  end
+
+  defp blocking_run(cmd) do
     :os.cmd(cmd |> String.to_charlist()) |> List.to_string()
+  end
+
+  defp streaming_run(cmd) do
+    list = String.split(cmd)
+    {bin, args} = List.pop_at(list, 0)
+    System.cmd(bin, args, into: IO.stream(:stdio, :line))
   end
 end
