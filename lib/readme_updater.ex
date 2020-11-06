91a355b352ab7028ee70f1380c50bf96f686e5ea
Roman Heinrich <roman.heinrich@gmail.com>
2020-11-06 22:30:55 +0100

    Sleep for 1/2 second when crawling readmes

--- lib/readme_updater.ex
+++ lib/readme_updater.ex
@@ -8,7 +8,11 @@ defmodule Updater.ReadmeUpdater do
   def run(urls) do
     stats_collection =
       urls
-      |> Parallel.run(10, fn url -> Updater.Crawler.stats(url) end)
+      |> Parallel.run(10, fn url ->
+        res = Updater.Crawler.stats(url)
+        :timer.sleep(500)
+        res
+      end)
       |> Enum.map(fn {_url, stats} -> stats end)
 
     Updater.ReadmeHandler.replace_activity!(stats_collection)
